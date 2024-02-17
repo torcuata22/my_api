c36165f1361ed417dcbbc26d3bede6d2c2efbbbd
Marilyn Marquez <marilynmarquez@Marilyns-MacBook-Pro.local>
2024-02-17 16:04:09 -0500

    added validations and tests for content and slug

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Article, type: :model do
   describe '#validations' do
     let(:article) {build(:article)}
+
     it "tests that factory is valid" do
       expect(article).to be_valid  #equivalent to article.valid? == true
     end
@@ -12,5 +13,18 @@ RSpec.describe Article, type: :model do
       expect(article).not_to be_valid
       expect(article.errors[:title]).to include("can't be blank")
     end
+
+      it 'has an invalid content' do
+        article.content = ''
+        expect(article).not_to be_valid
+        expect(article.errors[:content]).to include("can't be blank")
+      end
+
+        it 'has an invalid slug' do
+          article.slug = ''
+          expect(article).not_to be_valid
+          expect(article.errors[:slug]).to include("can't be blank")
+        end
+
   end
 end
