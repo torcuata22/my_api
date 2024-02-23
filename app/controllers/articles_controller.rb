e608fb27dd5924010be4dfc8eacd73f12cbcf644
Marilyn Marquez <marilynmarquez@Marilyns-MacBook-Pro.local>
2024-02-23 15:58:47 -0500

    refactored articles controller, created apginable module

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,32 +1,15 @@
 class ArticlesController < ActionController::API
 
+include Paginable
   def index
-    #articles = Article.all returned articles oldest to new
-    articles = Article.recent #returns articles in proper order: new to old
-    paginated = paginator.call(
-      articles,
-      params: pagination_params,
-      base_url: request.url
-    )
-    #article links, use .to_h to convert items into hash
-    options = { meta:paginated.meta.to_h, links: paginated.links.to_h }
-
+    paginated = paginate(Article.recent)
+    options = { meta:paginated.meta.to_h, links: paginated.links.to_h } #article links, use .to_h to convert items into hash
     render json: serializer.new(paginated.items, options), status: :ok
   end
 
   def serializer
     ArticleSerializer
   end
-
-  def paginator
-    JSOM::Pagination::Paginator.new
-  end
-
- def pagination_params
-  params.permit![:page]
- end
-
-
 
 
 end
