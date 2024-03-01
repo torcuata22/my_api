83625c185fda0911fa7e36484ebf206c62be09e1
Marilyn Marquez <marilynmarquez@Marilyns-MacBook-Pro.local>
2024-02-29 21:18:01 -0500

    access token test failure passed

--- app/controllers/access_tokens_controller.rb
+++ app/controllers/access_tokens_controller.rb
@@ -1,5 +1,5 @@
 class AccessTokensController < ApplicationController
   def create
-
+    render json:{}, status: 401
   end
 end
---
+++ spec/controllers/access_tokens_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe AccessTokensController, type: :controller do
+  describe 'create' do
+    context 'when invalid request' do
+      it 'should return 401 status code' do
+        post :create
+        expect(response).to have_http_status(401)
+
+      end
+    end
+
+    context 'when success request' do
+
+    end
+  end
+end
