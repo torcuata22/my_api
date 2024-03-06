e25f3405155915020f1a4d5c82b0607f50c9c6e5
Marilyn Marquez <marilynmarquez@Marilyns-MacBook-Pro.local>
2024-03-06 16:09:58 -0500

    refactoring

--- spec/controllers/access_tokens_controller_spec.rb
+++ spec/controllers/access_tokens_controller_spec.rb
@@ -2,55 +2,6 @@ require 'rails_helper'
 
 RSpec.describe AccessTokensController, type: :controller do
   describe '#create' do
-    shared_examples_for "unauthorized_requests" do
-      let(:authentication_error) do
-        {
-          "status" => "401",
-          "source" => { "pointer" => "/code" },
-          "title" => "Authentication code is invalid",
-          "detail" => "You must provide a valid code to exchange for a token"
-        }
-      end
-
-      it 'should return 401 status code' do
-        subject
-        expect(response).to have_http_status(401)
-      end
-
-      it 'should return proper error body' do
-        subject
-        parsed_response = JSON.parse(response.body)
-        expect(parsed_response['errors']).to include(authentication_error)
-      end
-    end
-
-
-    # shared_examples_for 'forbidden_requests' do
-    #   let(:authorization_error) do
-    #     {
-    #       "errors" => [{
-    #         "status" => "403",
-    #         "source" => { "pointer" => "/headers/authorization" },
-    #         "title" => "You are not authorized",
-    #         "detail" => "You are not authorized to access this resource"
-    #       }]
-    #     }
-    #   end
-
-    #   it 'should return 403 status code' do
-    #     subject
-    #     expect(response).to have_http_status(:forbidden)
-    #   end
-
-    #   it 'should return proper error json' do
-    #     subject
-    #     parsed_response = JSON.parse(response.body)
-    #     expect(parsed_response).to eq(authorization_error)
-    #   end
-    # end
-
-
-
     context 'when no code provided' do
       subject { post :create }
       it_behaves_like "unauthorized_requests"
@@ -104,31 +55,6 @@ RSpec.describe AccessTokensController, type: :controller do
   end
 
   describe 'DELETE #destroy' do
-    shared_examples_for 'forbidden_requests' do
-      let(:authorization_error) do
-        {
-          "errors" => [{
-            "status" => "403",
-            "source" => { "pointer" => "/headers/authorization" },
-            "title" => "You are not authorized",
-            "detail" => "You are not authorized to access this resource"
-          }]
-        }
-      end
-
-      it 'should return 403 status code' do
-        subject
-        expect(response).to have_http_status(:forbidden)
-      end
-
-      it 'should return proper error json' do
-        subject
-        parsed_response = JSON.parse(response.body)
-        expect(parsed_response).to eq(authorization_error)
-      end
-    end
-
-
     context 'when invalid request' do
       subject { delete :destroy }
       it_behaves_like 'forbidden_requests'
